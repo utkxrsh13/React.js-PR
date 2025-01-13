c7dd5dae1b5c29b6d7e6d2e40f9e07686118f85f
Utkarsh Tiwari <utkarsht0813@gmail.com>
2025-01-13 05:51:24 +0530

    vj

--- task 1/src/App.jsx
+++ task 1/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [fullname, setFullname ] = useState('')
+  const [email, setEmail ] = useState('')
+  const [password, setPassword ] = useState('')
+  const [confirm, setConfrim ] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log("e");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='h-screen flex items-center justify-center'>
+      <div className='bg-white rounded-xl p-6 w-96'>
+        <form onSubmit={(e)=>{
+          submitHandler(e)
+        }} className='text-black flex flex-col items-center gap-2'>
+          <input className='w-full border-2 border-blue-300 rounded px-3' type="text" placeholder='Enter your name here' />
+          <input className='w-full border-2 border-blue-300 rounded px-3' type="email" placeholder='Enter your email' />
+          <input className='w-full border-2 border-blue-300 rounded px-3' type="password" placeholder='Enter your  password' />
+          <input className='w-full border-2 border-blue-300 rounded px-3' type="password" placeholder='Confirm password' />
+          <button className='bg-emerald-200 px-3 py-1 text-black font-semibold rounded mt-5 w-full'>Submit</button>
+        </form>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
